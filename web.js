62bb6ba844275a64c134689d5c6eb78ee62e8e53
Luix70 <lortuno1@gmail.com>
2019-01-05 12:17:33 +0100

    Cambiado puerto a 81

--- web.js
+++ web.js
@@ -1,16 +1,12 @@
 
 var express = require("express");
-var fs = require("fs");
-var MongoClient = require("mongodb").MongoClient
-var assert = require("assert");
-var engines=require("consolidate");
+//var engines=require("consolidate");
 var app=express();
-var url = "mongodb://localhost:27017";
-var urlServer ="http://localhost:3000/"
 
-app.engine("html",engines.nunjucks);
-app.set("view engine", "html");
-app.set("views", __dirname+"/vistas");
+
+// app.engine("html",engines.nunjucks);
+// app.set("view engine", "html");
+// app.set("views", __dirname+"/vistas");
 
 app.use(express.static(__dirname)); //para poder referenciar las imagenes al directorio raiz
 
@@ -19,13 +15,13 @@ app.use(express.static(__dirname)); //para poder referenciar las imagenes al dir
 
 
 app.get("/",(req,res)=>{
-    res.render("index.html");
+  //res.sendFile(__dirname + "/index.html");
 });
 
 
-app.listen(80,()=>{
+app.listen(81,()=>{
     
-    console.log("te escucho en el puerto 80");
+    console.log("te escucho en el puerto 81");
 
     
 });
