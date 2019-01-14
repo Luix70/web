898ac9e018e9f27c7ce2615d160073c35370af2e
Luix70 <lortuno1@gmail.com>
2019-01-14 17:16:37 +0100

    A vueltas con cors

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
 var express = require("express");
+var fs=require("fs");
 var app=express();
+var https = require ("https");
+
+var port = 80;
+var sPort = 443;
+
+if (NODE_ENV =  "development") {
+    port = 1080;
+    sPort=1443
+}
+
 app.use(express.static(__dirname)); //para poder referenciar las imagenes al directorio raiz
-app.listen(1080,()=>{
-    console.log("te escucho en el puerto 1080");
+app.listen(port,()=>{
+    console.log(`te escucho en el puerto ${port}`);
 });
 
 
 
+var httpsOptions={
+    key: fs.readFileSync("./https/key_io.pem"),
+    cert:  fs.readFileSync("./https/cert_io.pem")
+}
+https.createServer(httpsOptions , app).listen(sPort,()=>{
+    console.log(`Escuchando (https) en el puerto: ${sPort}`)
+})
