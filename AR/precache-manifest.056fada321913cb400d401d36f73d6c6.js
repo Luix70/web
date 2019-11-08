07d4527b5c25c555767730e35b0316717c472c39
Luix70 <lortuno1@gmail.com>
2019-11-08 18:26:46 +0100

    https?

---
+++ AR/precache-manifest.056fada321913cb400d401d36f73d6c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d408ea7e6c9b78f1dc9d68e3a6d4d135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50491446ec858650a3ba",
+    "url": "/static/css/2.0b267439.chunk.css"
+  },
+  {
+    "revision": "fa01c0a8980a4a4fe35e",
+    "url": "/static/css/main.38dc32de.chunk.css"
+  },
+  {
+    "revision": "50491446ec858650a3ba",
+    "url": "/static/js/2.b2dd381b.chunk.js"
+  },
+  {
+    "revision": "fa01c0a8980a4a4fe35e",
+    "url": "/static/js/main.4f676772.chunk.js"
+  },
+  {
+    "revision": "6392e0549b37f586ad83",
+    "url": "/static/js/runtime-main.ea86e790.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
